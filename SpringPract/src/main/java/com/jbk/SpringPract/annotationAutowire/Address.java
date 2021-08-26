39afce34d473a2f90aa34f0cebc0a99cae85e45f
DeepakSalunke7 <88612758+DeepakSalunke7@users.noreply.github.com>
2021-08-26 17:25:28 +0530

    Add files via upload

---
+++ SpringPract/src/main/java/com/jbk/SpringPract/annotationAutowire/Address.java
@@ -0,0 +1,41 @@
+package com.jbk.SpringPract.annotationAutowire;
+
+public class Address {
+
+	public String street;
+	public String city;
+	
+	public Address() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Address(String street, String city) {
+		super();
+		this.street = street;
+		this.city = city;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [street=" + street + ", city=" + city + "]";
+	}
+
+	
+	
+	
+}
---
+++ SpringPract/src/main/java/com/jbk/SpringPract/annotationAutowire/Test.java
@@ -0,0 +1,15 @@
+package com.jbk.SpringPract.annotationAutowire;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test 
+{
+    public static void main( String[] args ){
+    	
+    	ApplicationContext context= new ClassPathXmlApplicationContext("com/jbk/SpringPract/annotationAutowire/autoConfig.xml");
+    	Employee emp1=context.getBean("emp1",Employee.class);
+    	System.out.println(emp1);
+    }
+}
